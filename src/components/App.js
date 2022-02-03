f8bad04403c177cef1ca3a93a267c25ea65be734
iremumut <irem2000.nur@gmail.com>
2022-02-04 00:01:31 +0300

    fetched data

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+
+import { useEffect, useState } from "react";
+
+function App() {
+  const [images, setImages] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img src={images} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
